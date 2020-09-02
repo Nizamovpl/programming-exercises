0a8190eddba8fe62136271b0cbca9ef003ba5cdd
Percy Nizamov <Mysticgummibear@gmail.com>
2020-09-02 11:14:22 -0600

    Shortfall complete

--- 8/Shortfall.java
+++ 8/Shortfall.java
@@ -1,5 +1,11 @@
 public class Shortfall{
-    
-
+    public static void main (String[] args ){
+        short value = 32; 
+        System.out.println("A short :" + value); 
+    }
 
 }
+
+//short is a primative data type
+//short only works for smaller numerical values @ requires less bits
+//int works for larger numbers
