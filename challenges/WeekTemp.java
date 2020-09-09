3774535f4eb686c32a73b1e4f8349abf7d5b6fa8
Percy Nizamov <Mysticgummibear@gmail.com>
2020-09-09 11:15:33 -0600

    Finished Challenge WeekTemp

--- challenges/WeekTemp.java
+++ challenges/WeekTemp.java
@@ -2,52 +2,40 @@ import java.util.Scanner;
 
 //Boring one in case my other one fails
 public class WeekTemp{
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in); 
+    public static void main(final String[] args){
+        final Scanner scanner = new Scanner(System.in); 
 
         System.out.println("What is Sunday's tempurature?"); 
-        String sun = scanner.next(); 
+        final String sun = scanner.next(); 
         System.out.println("What is Mondays's tempurature?"); 
-        String mon = scanner.next();
+        final String mon = scanner.next();
         System.out.println("What is Tuesdays's tempurature?");
-        String tues = scanner.next(); 
+        final String tues = scanner.next(); 
         System.out.println("What is Wendsdays's tempurature?");
-        String wed = scanner.next(); 
+        final String wed = scanner.next(); 
         System.out.println("What is Thursdays's tempurature?");
-        String thur = scanner.next(); 
+        final String thur = scanner.next(); 
         System.out.println("What is Fridays's tempurature?");
-        String fri = scanner.next(); 
+        final String fri = scanner.next(); 
         System.out.println("What is Saturdays's tempurature?");
-        String sat = scanner.next(); 
+        final String sat = scanner.next(); 
 
         System.out.println("Your weekly calender is : \n Sunday" + sun + "\n Monday:" + mon + "\n Tuesday:" + tues + "\n Wednsday:" + wed + "\n Thursday:" + thur + "\n Friday:" + fri + "\n Saturday:" + sat); 
+
+        final int intsun = Integer.parseInt(sun);
+        final int intmon = Integer.parseInt(mon);
+        final int inttues = Integer.parseInt(tues);
+        final int intwed = Integer.parseInt(wed); 
+        final int intthur = Integer.parseInt(thur);
+        final int intfri = Integer.parseInt(fri);
+        final int intsat = Integer.parseInt(sat);
+
+        int average = 0; 
+        
+        average = (intsun + intmon + inttues + intwed + intthur + intfri+ intsat)/7 ; 
+        
+        System.out.println("The average tempurature for the week is " + average); 
         
     }
 
-
-
-
 }
-
-//tempurature measured to the nearest degree are entered
-//boring method in case the advanced one fails
-
-
-
-// import java.util.Scanner;
-
-// public class HelloWorld {  //always there. the argument has to match the file name
-//     public static void main(String[] args){
-
-//         Scanner scanner = new Scanner(System.in); //2nd scanner is a variable
-
-//         System.out.println("Hello World!:>");
-//         System.out.println("\t and no you aren't getting my schedule because I am lazy. <-< Why do you want it anyways.");  
-        
-//         System.out.println("What's ur name");
-
-//         String placeholder = scanner.next(); 
-//         System.out.println("Hello " + placeholder); 
-//     }
-
-// }
---
+++ test.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class test {  //always there. the argument has to match the file name
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in); //2nd scanner is a variable
+
+        System.out.println("Hello World!:>");
+        System.out.println("\t and no you aren't getting my schedule because I am lazy. <-< Why do you want it anyways.");  
+        
+        System.out.println("What's ur name");
+
+        String placeholder = scanner.next(); 
+        System.out.println("Hello " + placeholder); 
+    }
+
+}
