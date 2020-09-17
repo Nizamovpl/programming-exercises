805d28a0524cb05f5368c6ff988e57207e2021b4
Percy Nizamov <Mysticgummibear@gmail.com>
2020-09-16 20:06:35 -0600

    Completed Discount prices and orderChecker

--- 16/DiscountPrices.java
+++ 16/DiscountPrices.java
@@ -1,30 +1,18 @@
-// Exercise 1 — Discount Prices
-// During a sale at a store, a 10% discount is applied to purchases over $10.00.
-// Write a program that asks for the amount of a purchase, 
-//then calculates the discounted price. The purchase amount will be input in cents (as an integer):
-
-// Enter amount of purchases:
-// 2000
-// Discounted price: 1800
-// Use integer arithmetic throughout the program.
-
-// Click here to go back to the main menu.
-
 import java.util.Scanner;
 
 public class DiscountPrices{
-
     public static void main(String[] agrs){
-        System.out.println("What is the total price of your purchase");
+        Scanner scanner = new Scanner(System.in);
 
-        //price = scanner.nextint();
-        
-        if(price < 10.00){
-
+        System.out.println("What is the total price of your purchase"); 
+        int price; 
+        price = scanner.nextInt(); 
+        if(price > 10.00){
+            System.out.println(price); 
+            price = price*(9/10); 
+            System.out.println("The discounted price is: "+price); 
+        }else{
+            System.out.println("There is no discount"); 
         }; 
-      
     }
-
-
-
 }
