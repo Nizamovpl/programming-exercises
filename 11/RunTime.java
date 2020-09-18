946c3220c79e3560c4ecd9cd2e7ac375c9f40bcd
Percy Nizamov <Mysticgummibear@gmail.com>
2020-09-18 06:59:10 -0600

    finished eleven

---
+++ 11/RunTime.java
@@ -0,0 +1,23 @@
+public class RunTime{
+    public static void main(String[] args){
+        String one; 
+        String two; 
+
+        one = "           AAAAAAAAAAAAHHHHHHHHHHhhhhh    AAAAAAAAAHHHHHHHHHHH    OHHHHHHHHHH   NOOOOOOOOOOOOOOOOO           ";
+        two = one.trim();  //keeps the spaces in the middle
+
+        System.out.println(one);
+        System.out.println(two); 
+
+
+
+    }
+
+
+
+
+
+}
+
+
+
