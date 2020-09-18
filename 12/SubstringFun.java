af8607873563807bad25d035d80d7554a3436ead
Percy Nizamov <Mysticgummibear@gmail.com>
2020-09-18 10:39:56 -0600

    finished 12

--- 12/SubstringFun.java
+++ 12/SubstringFun.java
@@ -1,18 +1,23 @@
-// Review Exercise 4 from the previous chapter. It describes the substring() method of class String
+import java.util.Scanner; 
 
-// String newString = originalString.substring( int beginIndex, int endIndex )
-// This method creates a new String object based on the original object, but containing only the characters that start at beginIndex and end at endIndex-1. It creates a completely new object by selectively copying characters from the original string.
+public class SubstringFun{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in); 
+        int begin;
+        int end;  
+        String sub; 
+        String string; 
+        System.out.println("Enter Beginning index"); 
+        begin = scanner.nextInt(); 
+        System.out.println("Enter End index");
+        end = scanner.nextInt(); 
+        System.out.println("Enter String");
+        string = scanner.next(); 
+        //add precautions against people entering more than value
+        //at a later date, maybe this weekend. But it's done :D
+        sub = string.substring(begin, end);  
+        System.out.println(sub); 
+    }
 
-// Write a program that asks the user for a string, a beginning index, and an ending index and then writes out the original string and the new substring:
 
-// Enter a string:
-// Eva, can I stab bats in a cave?
-// Enter beginning index: 5
-// Enter ending index: 20
-
-// Original string:
-// Eva, can I stab bats in a cave?
-
-// Substring:
-// can I stab bats
-// Use Scanner.nextLine() to read in the user's string.
+}
