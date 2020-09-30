1ed574a94142c86cf155f2fb5c7b7e77df546d87
Percy Nizamov <Mysticgummibear@gmail.com>
2020-09-30 10:16:18 -0600

    Rearranged folders

--- 10-20/13/HarmonicMean.java
+++ 10-20/13/HarmonicMean.java
@@ -9,23 +9,20 @@ public class HarmonicMean{
        double m; 
        //I might go back and test doubles later and change types for accuracy
 
+       //make the code resistant to change. If elements were to change. 
+       //anticipate problems
+       //if someone changes the code; then it shou;d stay
        Scanner scanner = new Scanner(System.in); 
        System.out.println("X: ");
        x = scanner.nextInt(); 
        System.out.println("Y: ");
-       y = scanner.nextInt(); 
+       y = scanner.nextInt();
+       
+       scanner.close(); 
 
        m = (x+y+0.0)/2;  
        h = (double)2/(1/x+1/y); 
        System.out.println("The Harmonic Mean: " + h); 
        System.out.println("The Mean: " + m); 
-
-
-
-
-
     }    
-
-
-
 }
