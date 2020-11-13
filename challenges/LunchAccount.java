b86bba3bf98585574b9d59cd1cf8492991647c80
Percy Nizamov <Mysticgummibear@gmail.com>
2020-11-13 10:43:07 -0700

    progress update

---
+++ challenges/LunchAccount.java
@@ -0,0 +1,21 @@
+public class LunchAccount{
+    private int currentBalence; 
+
+    //checks balence. 
+
+    StudentDta student = new Student("Student"); //find out syntax
+
+    public void addMoney(student){
+        student.get.balence; ///ahhh I forgot the syntax; 
+        currentBalence = currentBalence + balence;
+        
+
+    }
+
+    public get getCurrentBalance(){
+        return currentBalence; //add a student acess route for it 
+    }
+
+
+}
+
---
+++ challenges/StudentDta.java
@@ -0,0 +1,37 @@
+//I mispelled data
+//Holds current balemce
+public class StudentDta{
+    public static int id = 1000 ;
+    private int addedBalance; 
+    private boolean isTwentyAdded = false; 
+
+    public StudentDta(){
+        this.id = id + 1; 
+        this.addedBalance = 0; 
+    }
+
+    
+    public StudentDta(addedBalance){
+        this.id = id + 1; 
+
+        if(isTwentyAdded = false && id < 1101){
+            addedBalance = addedBalance + 20; 
+            isTwentyAdded = true
+        }else{
+           //void 
+        }; 
+
+        //how do I make it so that I can add $20 one time, and not call the class. 
+        this.addedBalance = addedBalance; 
+    }
+
+
+
+
+
+
+
+}
+
+//I spent my entire time figuring out where everything should go class wise, like where to put the student
+//balence and so and oso. I'm a slow coder as I like to think before doing a task. 
