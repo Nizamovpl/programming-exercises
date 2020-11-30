ce29fabb5294835eb7a4abc36a111a8be0bb3c37
Percy Nizamov <Mysticgummibear@gmail.com>
2020-11-30 11:16:03 -0700

    progress 3

--- files/IO.java
+++ files/IO.java
@@ -1,40 +1,32 @@
 import java.util.Scanner;
-import java.io.*; 
+import java.io.*;
 
-public class IO{
+public class IO {
+    public static void main(String[] args){
+        System.out.println("Input/Output Example ");
 
-    public static void main(String[] args) throws IOException, FileNotFoundException{
-        System.out.println("Input/Output example"); 
-        
+        File file;
+        Scanner scan;
 
-        // try{
-        //   file = new File(wrong.csv); 
-        //   scan = new File(file);   
-        // }catch(IOExeption e){
-            //System.out.println("error");
-            //System.exit(0);  
-        //}catch(FileNotFound e){
-            //System.exit(0);  
-        //}catch(Exeption e){
-            //System.exit(0);  
-            //e.printStackTrace; 
-        //}finally(){
-            //System.out.println(done);  
+        //try/cash/finally --> we manage it ourselves
 
-        //}
+        //can do scan has next line
+        try {
+            file = new File("wrong.csv");
+            scan = new Scanner(file);
 
-        //exeption is an object, that deals with exactly gonna happen
-        File file = new File("data");
-        //exeption - more sepcific expeption --> IO exeption
-        Scanner scanner = new Scanner(file);  
-        //throws = can't handle code
-        //trycatch statement = we manage it ourself. 
-
-        int num = scanner.nextInt(); 
-        System.out.println("The first number is" + num); 
+            while(scan.hasNextInt()){
+                int num = scan.nextInt();
+                System.out.println("The next number is " + num); 
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            System.out.println("Done");
+        }
     }
-
 }
 
 
-
+//go back in git commits, since they have different variants: this one is a retyped version. 
+//different ways of doing it. Tis a bit annoying but will do
