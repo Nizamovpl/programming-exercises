0d4f357637850fbd8a0fdd0a460c70665d5b0848
Percy Nizamov <Mysticgummibear@gmail.com>
2021-01-19 11:11:56 -0700

    notes

--- inheritence/Dog.java
+++ inheritence/Dog.java
@@ -1,53 +1,5 @@
-public class Dog{
+public class Dog extends Animal{
 
-    private String name,breed; 
-    private int age; 
-
-    public Dog(String name, String breed, int age){
-        this.name = name; 
-        this.breed = breed; 
-        this.age = age; 
-    }
-  
-    private boolean isEat = false;
-    private boolean isplay = false; 
-    private  boolean isSleep = false; 
-    
-    public void eat(){
-        isEat = true; 
-    }
-
-    public void play(){
-        isplay = true; 
-    }
-
-    public void sleep(){
-        isSleep = true; 
-    }
-
-    public String bark(){
-        return "bark"; 
-    }
-
-    public boolean hasEaten(){
-        return isplay; 
-    }
-
-    public Boolean hasPlayed(){
-        return isplay; 
-    }
-
-    public Boolean hasSlept(){
-        return isSleep; 
-    }
-
-    public Boolean isHappy(){
-
-        if(isEat || isSleep || isplay){
-            return true; 
-        }else{
-            return false; 
-        }
-    }
+   
 
 }
