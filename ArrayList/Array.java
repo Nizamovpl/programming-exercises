e82c540d73766dddf282213e1181f5b72c1beeeb
Percy Nizamov <Mysticgummibear@gmail.com>
2021-01-28 12:52:39 -0700

    framework

---
+++ ArrayList/Array.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Array{
+
+    ArrayList<String>fruits= new ArrayList<String>(); 
+    ArrayList<String>vegetables= new ArrayList<String>(); 
+    ArrayList<String>fruitandVeg= new ArrayList<String>(); 
+}
